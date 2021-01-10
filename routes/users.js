4f92075019214ca6f4d60871c4b4334b18f067c4
anusu90 <anusu90@gmail.com>
2021-01-10 19:29:42 +0530

    second commit

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,16 @@ var router = express.Router();
 var jwt = require('jsonwebtoken')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
+  console.log(req.headers)
+
   let jwt2 = jwt.sign("hello", "asdasdasdasdasdasdasdasdasdasd")
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Set-Cookie myAgainJwt");
-  res.setHeader("myAgainJwt", jwt2);
-  res.cookie("myAgainJwt", jwt2, {sameSite: 'lax', path: "/"})
-  console.log(res.headers);
-  res.json({message: 'respond with a resource',
-  myAgainJwt: res.cookie});
+  // res.setHeader("myAgainJwt", jwt2);
+  res.cookie("myAgainJwt2", jwt2, {sameSite: "none", secure:true})
+
+  res.json({"message": 'respond with a resource'});
 });
 
 module.exports = router;
